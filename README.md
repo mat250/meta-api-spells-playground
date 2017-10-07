ba7886726faf32d223063c118c7a378c1652410a
mat250 <mat250.alter@gmail.com>
2017-10-07 13:50:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Meta API - Spells Playground
+
+Meta API - Spells Playground is an app to help you to build **Meta API Spells** on your machine.
+
+More info about **Meta API Spells** here : https://meta-api.io/spells
+
+## How to use it
+
+Clone this repo on your computer
+
+Install all the dependancies with `npm install`
+
+Open it with your favorite editor. I've add some nice tools for VS Code ;)
+
+Build your code between the indicated notes. So, you'll just need to copy / paste this section to Meta API Spells Admin after.
+You have an example ready to use ;)
+
+For an easy dev, launch the app with `npm run dev` which will autoreload the app on changed.
+
+Then, just open the http://localhost:3000/ on your favorite browser or on tools like Postman.
+
+Meta API SDK is already included and configured. You can access to query parameters you put in the URL with `params` variable.
---
+++ src/index.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const app = express();
+var mapi_sdk = require('meta-api-sdk');
+
+app.get('/', function (req, res) {
+
+  mapi_sdk.config('dev'); //You'll be able to change the URL of Meta API SDK if you want to use a proxy
+
+  var params = req.query; //Query parameters will be accessible through "params";
+
+  //Simulating final execution environment
+  var simulatedEnv = function (callback) {
+    
+    /**
+     * Your code start here #########
+     */
+
+    mapi_sdk.import([
+      {
+        "id": 0,
+        "type": "Geo",
+        "params": [
+          {
+            "name": "address",
+            "value": "1 rue de Rivoli 75001 Paris"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "type": "Geo",
+        "params": [
+          {
+            "name": "address",
+            "value": "156 av des Champs-Elysées 75008 Paris"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "type": "Transport",
+
+        "params": [
+          {
+            "name": "start_latitude",
+            "connect_to": 0
+          },
+          {
+            "name": "start_longitude",
+            "connect_to": 0
+          },
+          {
+            "name": "end_latitude",
+            "connect_to": 1
+          },
+          {
+            "name": "end_longitude",
+            "connect_to": 1
+          }
+        ]
+      }
+    ]);
+
+    mapi_sdk.launch(function (error, result) {
+
+      if (error) {
+        callback(error, null);
+      }
+
+      let myResults = [];
+
+      //Creating new object
+      result.results.forEach(function (result) {
+        if (result.Transport != null) {
+          let transports = result.Transport
+          transports.forEach(function (transport) {
+            console.log(transport)
+            if (transport.fare != null && transport.name != null) {
+              //Clean price
+              let regexPrice = /(\d{1,2})-(\d{1,2})/g
+              let match = regexPrice.exec(transport.fare);
+              if (match != null) {
+                let price1 = parseInt(match[1]);
+                let price2 = parseInt(match[2]);
+                let average_price = (price1 + price2) / 2;
+                if (average_price != null) {
+                  myResults.push({
+                    price: average_price,
+                    name: transport.name
+                  })
+                }
+              }
+            }
+          }, this);
+        }
+      }, this);
+
+      //Sorting prices
+      myResults.sort((a, b) => {
+        if (a.price < b.price) return -1
+        else return 1
+      })
+
+      //Using params
+      if (params.max_price != null) {
+        let newResults = [];
+        myResults.forEach(function(result) {
+          if (result.price < params.max_price) {
+            newResults.push(result);
+          }
+        }, this);
+        myResults = newResults;
+      }
+
+      /**
+       * This fonction is mandatory to return your reusults
+       * First param is error to manage erros and the second is the result
+       */
+      callback(null, myResults);
+
+      /**
+       * Your code end here #######
+       */
+      
+    })
+  }
+
+  //Simulating results
+  simulatedEnv(function (error, result) {
+    if (error) {
+      res.status(500).send({success: false, msg: error});
+    } else {
+      res.json(result);
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Meta API - Spells Playground listening on port 3000!')
+});
